a891eb3c31d09ec0cd1b547e8f95c1151e7a7f98
Eric Otuvaka <eric.otuvaka@gmail.com>
2025-02-13 20:32:02 -0800

    freestanding binary with cross-compilation targets

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle] // don't mangle the name of this function in stack traces
+pub extern "C" fn _start() -> ! {
+    // this function is our entrypoint since the C linker
+    // looks for a '_start' function by default
+    loop {}
+}
+
+// Called on panic
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
