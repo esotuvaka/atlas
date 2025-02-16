bfbca7df7aace41823dfc4abfb2140b6a1dbef8d
Eric Otuvaka <eric.otuvaka@gmail.com>
2025-02-15 21:31:06 -0800

    feat: custom test framework

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 mod vga_buffer;
 
 use core::fmt::Write;
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle] // don't mangle the name of this function in stack traces
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Some panic message");
     // this function is our entrypoint since the C linker
     // looks for a '_start' function by default
     loop {}
 }
 
 // Called on panic
-#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
