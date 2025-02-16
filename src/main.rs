8b7a050f4f4b19e0062002859c88df6e7f218e21
Eric Otuvaka <eric.otuvaka@gmail.com>
2025-02-15 21:35:18 -0800

    wip: first tests + auto-shutdown qemu on tests completion

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
@@ -11,6 +12,9 @@ use core::panic::PanicInfo;
 #[no_mangle] // don't mangle the name of this function in stack traces
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
     // this function is our entrypoint since the C linker
     // looks for a '_start' function by default
     loop {}
@@ -29,4 +33,11 @@ pub fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(2 - 1, 1);
+    println!("[ok]");
 }
