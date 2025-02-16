0dc52af3d047de233fda80a29a70e9da55a88c4c
Eric Otuvaka <eric.otuvaka@gmail.com>
2025-02-15 22:50:04 -0800

    feat: unit and integration tests, library module, etc.

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,37 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(atlas::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 mod serial;
 mod vga_buffer;
 
-use core::fmt::Write;
 use core::panic::PanicInfo;
 
+// this function is our entrypoint since the C linker
+// looks for a '_start' function by default
 #[no_mangle] // don't mangle the name of this function in stack traces
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
     #[cfg(test)]
     test_main();
-    // this function is our entrypoint since the C linker
-    // looks for a '_start' function by default
+
     loop {}
 }
 
 // Called on panic
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
+// panic handler in test mode
 #[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
-    assert_eq!(2 - 1, 1);
-    serial_println!("[ok]");
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32)
-    }
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    atlas::test_panic_handler(info)
 }
